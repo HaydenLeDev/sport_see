1fcdc5894c05587b440580c01a8cf71cb5543812
HaydenLeDev <loicroth.pro@gmail.com>
2022-11-29 16:48:28 +0100

    mise en place

---
+++ src/components/header/Logo.js
@@ -0,0 +1,9 @@
+function Logo() {
+    return (
+        <div className="Logo">
+            Test
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/components/header/Menu.js
@@ -0,0 +1,9 @@
+function Menu() {
+    return (
+        <div className="Menu">
+            Test
+        </div>
+    );
+}
+
+export default Menu;
