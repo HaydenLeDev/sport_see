cdff3750b0e9505c45ea6ffb511a65c7c2d351fb
HaydenLeDev <loicroth.pro@gmail.com>
2022-12-01 11:17:16 +0100

    getUserById

--- sport_see_front/src/components/home/Home.js
+++ sport_see_front/src/components/home/Home.js
@@ -1,10 +1,12 @@
 import Header from "../header/Header";
 import Container from "./Container";
+import {getUserById} from "../../service/mock/apiMock"
 import '../../style/components/header/Header.css';
 
 function Home() {
 
-    const data = 0;
+    const data = getUserById(12);
+    console.log(data.userInfos.firstName)
 
     return (
         <div className="Home">
--- sport_see_front/src/service/mock/apiMock.js
+++ sport_see_front/src/service/mock/apiMock.js
@@ -1,1 +1,9 @@
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from "./dataMock"
+
+export function getUserById(id){
+    for(var i = 0; i < USER_MAIN_DATA.length; i++){
+        if (USER_MAIN_DATA[i].id === id){
+            return USER_MAIN_DATA[i]
+        }
+    }
+}
