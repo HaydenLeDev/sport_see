7b2d3542a03b87deea5ff4e994a538c572541c46
HaydenLeDev <loicroth.pro@gmail.com>
2023-01-04 16:57:09 +0100

    Update Container.js

--- sport_see_front/src/components/home/Container.js
+++ sport_see_front/src/components/home/Container.js
@@ -22,32 +22,6 @@ function Container() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() =>{
-        /*
-        const fetchData = async ()=>{
-            const infoUserResult = await axios(
-                "http://localhost:3000/user/" + id,
-            )   
-            setInfoUser(infoUserResult.data.data)
-
-            const userActivityResult = await axios(
-                "http://localhost:3000/user/"+ id + "/activity",
-            ) 
-            setUserActivity(userActivityResult.data.data)
-
-            const userAverageSessionsResult = await axios(
-                "http://localhost:3000/user/"+ id + "/average-sessions",
-            ) 
-            setUserAverageSessions(userAverageSessionsResult.data.data)
-
-            const userPerformanceResult = await axios(
-                "http://localhost:3000/user/"+ id + "/performance",
-            ) 
-            setUserPerformance(userPerformanceResult.data.data)
-
-            setLoading(false)
-        }
-        fetchData()
-        */
        const fetchData = async() => {
             const infoUserResult = await getUserByIdApi(id)
             setInfoUser(infoUserResult.data.data)
@@ -72,8 +46,6 @@ function Container() {
     //const userAverageSessions = getUserAverageSessions(parseInt(id))
     //const userPerformance = getUserPerformance(parseInt(id))
     
-    
-
     if(loading){
         return (
             <div className="Container">
@@ -81,15 +53,6 @@ function Container() {
             </div>
         )
     } else{
-        console.log("infoUser")
-        console.log(infoUser)
-        console.log("userActivity")
-        console.log(userActivity)
-        console.log("userAverageSessions")
-        console.log(userAverageSessions)
-        console.log("userPerformance")
-        console.log(userPerformance)
-
         const sessionUser = userActivity.sessions
         const isNiceDay = sessionUser[sessionUser.length - 1].calories > sessionUser[sessionUser.length - 2].calories
         return (
